4075655512d55da7f6f4aa586aae97a322f3ecd8
Dylan Bosch <iql237@usask.ca>
2025-06-01 15:35:04 -0600

    Finished chapter 6 material

--- Chap06/working.cpp
+++ Chap06/working.cpp
@@ -1,11 +1,29 @@
 // working.cpp by Bill Weinman [bw.org]
 // updated 2022-05-21
-#include <format>
 #include <iostream>
-
-using std::format;
 using std::cout;
 
+int nonRecursiveFact(int num){
+    int result {1};
+    
+    while(num >= 2){
+        result *= num;
+        --num;
+    }
+    
+    return result;
+}
+
 int main() {
-    cout << "Hello, World!\n";
+
+    auto test1 {5};
+    auto t1r = nonRecursiveFact(test1);
+    
+    cout << test1 << "! is: " << t1r << std::endl;
+
+    auto test2 {25};
+    auto t2r = nonRecursiveFact(test2);
+    
+    cout << test2 << "! is: " << t2r << std::endl;
+    
 }
