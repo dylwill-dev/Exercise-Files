af66e4109214356533b6adeed9d0779faa1e612f
Dylan Bosch <iql237@usask.ca>
2025-06-01 15:01:55 -0600

    Finished chapter 5 material

--- Chap04/working.cpp
+++ Chap04/working.cpp
@@ -1,7 +1,7 @@
 // working.cpp by Bill Weinman [bw.org]
 // updated 2022-05-21
 #include <iostream>
-using std::cout;
+using std:: cout;
 
 struct Book{
     const char *title {};
--- Chap05/operators.cpp
+++ Chap05/operators.cpp
@@ -1,14 +1,18 @@
 // operators.cpp by Bill Weinman [bw.org]
 // updated 2022-06-17
-#include <format>
 #include <iostream>
-
-using std::format;
 using std::cout;
 
 int main() {
     auto x = 5;
     auto y = 47;
-    cout << format("x is {}\n", x);
-    cout << format("y is {}\n", y);
+    cout << "X is: " << x << std::endl;
+    cout << "Y is: " << y << std::endl;
+
+    auto expression = x * y;
+
+    cout << "\n";
+    cout << "Expression is: " << expression << std::endl;
+
+
 }
